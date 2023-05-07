45a0fd15bda3aa8eeb0f1280a82459810e1db4a7
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-05-07 15:40:06 +0200

    Iterate through birthdays_dict using iterrows()

--- main.py
+++ main.py
@@ -4,32 +4,28 @@ import smtplib
 import os
 import random
 
-PLACEHOLDER = "[NAME]"
 MY_EMAIL = "YOUR EMAIL"
 MY_PASSWORD = "YOUR PASSWORD"
+PLACEHOLDER = "[NAME]"
 
 today = dt.datetime.today()
-today_month = int(today.strftime("%m"))
-today_day = int(today.strftime("%d"))
+today_month_day = (today.month, today.day)
 
 birthdays = pd.read_csv("birthdays.csv")
-birthday_month = int(birthdays["month"])
-birthday_day = int(birthdays["day"])
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in birthdays.iterrows()}
 
-receiver_email = birthdays.email
+if today_month_day in birthdays_dict:
+    receiver_email = birthdays_dict[today_month_day]["email"]
+    receiver_name = birthdays_dict[today_month_day]["name"]
+    random_template = random.choice(os.listdir("letter_templates"))
 
-receiver_name = birthdays["name"].to_string(index=False)
-random_template = random.choice(os.listdir("letter_templates"))
+    with open(f"./letter_templates/{random_template}", "r") as file:
+        template_contents = file.read()
+        template_to_send = template_contents.replace(PLACEHOLDER, receiver_name)
 
-with open(f"./letter_templates/{random_template}", "r") as file:
-    template_contents = file.read()
-    template_to_send = template_contents.replace(PLACEHOLDER, receiver_name)
-
-with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS", 587, timeout=120) as connection:
-    connection.starttls()
-    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-
-    if today_day == birthday_day and today_month == birthday_month:
+    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS", 587, timeout=120) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
         connection.sendmail(
                     from_addr=MY_EMAIL,
                     to_addrs=receiver_email,
